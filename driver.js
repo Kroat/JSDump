61120902b16d0e8a22e3afee2baf4a11e1407ccb
Matija Krolo <krolo@wisc.edu>
2021-12-02 13:02:08 -0600

    Update driver.js

--- driver.js
+++ driver.js
@@ -1,31 +1,9 @@
-var elemDiv = document.createElement('div');
-elemDiv.innerHTML = `<div id="modal-1" aria-hidden="true">
+(async() => { 
+    while(!window.hasOwnProperty("Swal")) // define the condition as you like
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    alert("Variable is defined");
+})();
 
-  <!-- [2] -->
-  <div tabindex="-1" data-micromodal-close>
-
-    <!-- [3] -->
-    <div role="dialog" aria-modal="true" aria-labelledby="modal-1-title" >
-
-
-      <header>
-        <h2 id="modal-1-title">
-          Modal Title
-        </h2>
-
-        <!-- [4] -->
-        <button aria-label="Close modal" data-micromodal-close></button>
-      </header>
-
-      <div id="modal-1-content">
-        Modal Content
-      </div>
-
-    </div>
-  </div>
-</div>`
-document.body.appendChild(elemDiv);
-MicroModal.init();
 // function httpGet(url) {
 //     var xmlHttp = new XMLHttpRequest();
 //     xmlHttp.open("GET", url, false); // false for synchronous request
