3fd6e52b01a25b7c0b666b861d58c695217e2429
Matija Krolo <krolo@wisc.edu>
2021-12-05 21:52:56 -0600

    Update drivr.js

--- drivr.js
+++ drivr.js
@@ -7,7 +7,7 @@ function init(){
         var entries = performance.getEntriesByType('resource');
         entries.map(function(entry) {
           if (entry.initiatorType === 'script') {
-            if(entry.name.includes('https://cdn.statically.io/gh/Kroat/JSDump/main/drv.js')){
+            if(entry.name.includes('https://cdn.statically.io/gh/Kroat/JSDump/main/drivr.js')){
                 _target = entry.name;
                 return;
             }
