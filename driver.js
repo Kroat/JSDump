2a8ac9ac39c94877dac04274db55e28d8c4d215f
Matija Krolo <krolo@wisc.edu>
2021-12-05 21:56:43 -0600

    Update driver.js

--- driver.js
+++ driver.js
@@ -7,7 +7,7 @@ function init(){
         var entries = performance.getEntriesByType('resource');
         entries.map(function(entry) {
           if (entry.initiatorType === 'script') {
-            if(entry.name.includes('https://cdn.statically.io/gh/Kroat/JSDump/main/drivr.js')){
+            if(entry.name.includes('https://cdn.statically.io/gh/Kroat/JSDump/main/driver.js')){
                 _target = entry.name;
                 return;
             }
